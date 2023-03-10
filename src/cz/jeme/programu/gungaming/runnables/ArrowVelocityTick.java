b0d97e0c5bcb6770e8287295861237d632e56248
XD_CZ <mandlemankiller@email.cz>
2023-03-10 21:15:38 +0100

    Remove arrow after 30 seconds of lifetime

--- src/cz/jeme/programu/gungaming/runnables/ArrowVelocityTick.java
+++ src/cz/jeme/programu/gungaming/runnables/ArrowVelocityTick.java
@@ -17,6 +17,10 @@ public class ArrowVelocityTick extends BukkitRunnable implements ArrowVelocityLi
 	public void run() {
 		for (Arrow arrow : arrows.keySet()) {
 			arrow.setVelocity(arrows.get(arrow));
+			if (arrow.getTicksLived() >= 600) { // After 30 seconds
+				removeArrow(arrow);
+				arrow.remove();
+			}
 		}
 	}
 
