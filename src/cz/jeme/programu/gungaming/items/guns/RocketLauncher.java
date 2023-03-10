c95f604dc8c70758391974ee712abc43ef2af038
XD_CZ <mandlemankiller@email.cz>
2023-03-10 21:16:44 +0100

    Update Rocket launcher

--- src/cz/jeme/programu/gungaming/items/guns/RocketLauncher.java
+++ src/cz/jeme/programu/gungaming/items/guns/RocketLauncher.java
@@ -8,13 +8,12 @@ public class RocketLauncher extends Gun {
 	protected void setup() {
 		name = "Rocket Launcher";
 		loreLine = "Awesome launchers";
-		shootCooldown = 5000;
-		reloadCooldown = 10000;
+		shootCooldown = 2000;
+		reloadCooldown = 7000;
 		damage = 15d;
 		velocity = 1.5f;
-//		velocity = 0.1f;
 		material = Material.DIAMOND_HOE;
-		maxAmmo = 999;
+		maxAmmo = 1;
 		isRocket = true;
 		ammoName = "Rocket";
 	}
