5e5a67604df2fee5de8820a16409a393f610bfb7
Mandlemankiller <mandlemankiller@email.cz>
2024-08-06 10:09:18 +0200

    exclude backup files

--- deploy-resource-pack.sh
+++ deploy-resource-pack.sh
@@ -2,18 +2,19 @@
 
 RESOURCE_PACK_FOLDER="resource-pack"
 OUTPUT_FOLDER="build"
-HASH_FILE=${OUTPUT_FOLDER}/resource-pack.sha1
-ZIP_FILE=${OUTPUT_FOLDER}/resource-pack.zip
+HASH_FILE="${OUTPUT_FOLDER}/resource-pack.sha1"
+ZIP_FILE="${OUTPUT_FOLDER}/resource-pack.zip"
 
-mkdir ${OUTPUT_FOLDER} >/dev/null 2>&1
+mkdir "${OUTPUT_FOLDER}" >/dev/null 2>&1
 
 rm "${HASH_FILE}" >/dev/null 2>&1
 rm "${ZIP_FILE}" >/dev/null 2>&1
 
-cd ${RESOURCE_PACK_FOLDER} \
-&& zip -r ../"${ZIP_FILE}" . -x '*.kra' '*.xcf' \
-&& cd ..
+# cd: zip only the contents of the resource pack folder, not the folder itself
+cd "${RESOURCE_PACK_FOLDER}" && \
+zip -r "../${ZIP_FILE}" . -x '*.kra' '*.xcf' '*~' && \
+cd ..
 
-echo Generating SHA-1...
+echo "Generating SHA-1..."
 sha1sum "${ZIP_FILE}" | awk '{ print $1 }' | tr -d '\n' >"${HASH_FILE}"
-echo Done!
+echo "Done!"
