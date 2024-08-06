e317a463107c26d0e69d0118dc8e2403c7981d0f
Mandlemankiller <mandlemankiller@email.cz>
2024-08-06 09:57:52 +0200

    exclude gimp and krita files in deployment

--- deploy-resource-pack.sh
+++ deploy-resource-pack.sh
@@ -10,7 +10,9 @@ mkdir ${OUTPUT_FOLDER} >/dev/null 2>&1
 rm "${HASH_FILE}" >/dev/null 2>&1
 rm "${ZIP_FILE}" >/dev/null 2>&1
 
-cd ${RESOURCE_PACK_FOLDER} && zip -r ../"${ZIP_FILE}" . && cd ..
+cd ${RESOURCE_PACK_FOLDER} \
+&& zip -r ../"${ZIP_FILE}" . -x '*.kra' '*.xcf' \
+&& cd ..
 
 echo Generating SHA-1...
 sha1sum "${ZIP_FILE}" | awk '{ print $1 }' | tr -d '\n' >"${HASH_FILE}"
