e7b3c2fff8fb2faf714d2f8a52af71d84da8af1c
Mandlemankiller <mandlemankiller@email.cz>
2024-08-08 23:29:01 +0200

    fix (reverse) tracker textures

--- src/main/java/cz/jeme/programu/gungaming/ResourcePackEventHandler.java
+++ src/main/java/cz/jeme/programu/gungaming/ResourcePackEventHandler.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.UUID;
 
 public final class ResourcePackEventHandler {
-    private static final boolean RESOURCE_PACK_ENABLED = true;
+    private static final boolean RESOURCE_PACK_ENABLED = false;
 
     private static final @NotNull Component RESOURCE_PACK_MESSAGE = Components.of("""
                     <b><dark_blue>============================================================</b>
