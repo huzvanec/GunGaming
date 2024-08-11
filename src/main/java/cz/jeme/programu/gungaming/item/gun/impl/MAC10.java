189c49928077aa2e8aa2eb9462cac1bc26c7057a
Mandlemankiller <mandlemankiller@email.cz>
2024-08-11 16:48:19 +0200

    add MAC-10

--- src/main/java/cz/jeme/programu/gungaming/item/gun/impl/MAC10.java
+++ src/main/java/cz/jeme/programu/gungaming/item/gun/impl/MAC10.java
@@ -1,44 +1,44 @@
 package cz.jeme.programu.gungaming.item.gun.impl;
 
 import cz.jeme.programu.gungaming.item.ammo.Ammo;
-import cz.jeme.programu.gungaming.item.ammo.impl.TwelveGauge;
-import cz.jeme.programu.gungaming.item.attachment.disable.MagazineDisabled;
+import cz.jeme.programu.gungaming.item.ammo.impl.NineMillimeter;
+import cz.jeme.programu.gungaming.item.attachment.disable.ScopeDisabled;
 import cz.jeme.programu.gungaming.item.gun.Gun;
 import cz.jeme.programu.gungaming.loot.Rarity;
 import net.kyori.adventure.key.KeyPattern;
 import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
-public class RemingtonM870 extends Gun implements MagazineDisabled {
+public class MAC10 extends Gun implements ScopeDisabled {
 
     @Override
     protected int provideMaxAmmo() {
-        return 5;
+        return 32;
     }
 
     @Override
     protected int provideShootCooldown() {
-        return 25;
+        return 4;
     }
 
     @Override
     protected int provideReloadDuration() {
-        return 13;
+        return 32;
     }
 
     @Override
     protected double provideDamage() {
-        return 2.3;
+        return 2;
     }
 
     @Override
     protected double provideBulletVelocity() {
-        return 5;
+        return 6;
     }
 
     @Override
     protected double provideRecoil() {
-        return .05;
+        return .02;
     }
 
     @Override
@@ -48,46 +48,41 @@ public class RemingtonM870 extends Gun implements MagazineDisabled {
 
     @Override
     protected @NotNull Class<? extends Ammo> provideAmmoType() {
-        return TwelveGauge.class;
-    }
-
-    @Override
-    protected @KeyPattern.Value @NotNull String provideKey() {
-        return "remington_m870";
-    }
-
-    @Override
-    protected @NotNull Component provideName() {
-        return Component.text("Remington M870");
+        return NineMillimeter.class;
     }
 
     @Override
     protected @NotNull String provideDescription() {
-        return "Good shotgun for close-range";
+        return "high fire rate SMG with low accuracy";
     }
 
     @Override
-    protected int provideBulletsPerShot() {
-        return 9;
-    }
-
-    @Override
-    protected int provideBulletCooldown() {
-        return 0;
-    }
-
-    @Override
-    protected boolean provideMagazineless() {
-        return true;
-    }
-
-    @Override
-    protected @NotNull Integer provideCustomModelData() {
-        return 6;
+    protected @KeyPattern.Value @NotNull String provideKey() {
+        return "mac-10";
     }
 
     @Override
     protected @NotNull Rarity provideRarity() {
         return Rarity.RARE;
+    }
+
+    @Override
+    protected @NotNull Component provideName() {
+        return Component.text("MAC-10");
+    }
+
+    @Override
+    protected int provideBulletsPerShot() {
+        return 4;
+    }
+
+    @Override
+    protected int provideBulletCooldown() {
+        return 1;
+    }
+
+    @Override
+    protected @NotNull Integer provideCustomModelData() {
+        return 11;
     }
 }
