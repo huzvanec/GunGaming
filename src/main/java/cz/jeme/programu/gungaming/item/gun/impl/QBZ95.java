46b7433451da5d34b2634a93184ee14a79a33778
Mandlemankiller <mandlemankiller@email.cz>
2024-09-02 17:57:17 +0200

    add QBZ-95

---
+++ src/main/java/cz/jeme/programu/gungaming/item/gun/impl/QBZ95.java
@@ -0,0 +1,86 @@
+package cz.jeme.programu.gungaming.item.gun.impl;
+
+import cz.jeme.programu.gungaming.item.ammo.Ammo;
+import cz.jeme.programu.gungaming.item.ammo.impl.FiveFiveSixMillimeter;
+import cz.jeme.programu.gungaming.item.gun.Gun;
+import cz.jeme.programu.gungaming.loot.Rarity;
+import net.kyori.adventure.key.KeyPattern;
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
+
+public class QBZ95 extends Gun {
+    @Override
+    protected int provideMaxAmmo() {
+        return 75;
+    }
+
+    @Override
+    protected int provideShootCooldown() {
+        return 4;
+    }
+
+    @Override
+    protected int provideReloadDuration() {
+        return 75;
+    }
+
+    @Override
+    protected double provideDamage() {
+        return 3.3;
+    }
+
+    @Override
+    protected double provideBulletVelocity() {
+        return 6;
+    }
+
+    @Override
+    protected double provideRecoil() {
+        return .045;
+    }
+
+    @Override
+    protected double provideInaccuracy() {
+        return .5;
+    }
+
+    @Override
+    protected @NotNull Class<? extends Ammo> provideAmmoType() {
+        return FiveFiveSixMillimeter.class;
+    }
+
+    @Override
+    protected @NotNull String provideDescription() {
+        return "high dps LMG with a large magazine capacity";
+    }
+
+    @Override
+    protected @KeyPattern.Value @NotNull String provideKey() {
+        return "qbz-95";
+    }
+
+    @Override
+    protected @NotNull Rarity provideRarity() {
+        return Rarity.LEGENDARY;
+    }
+
+    @Override
+    protected @NotNull Component provideName() {
+        return Component.text("QBZ-95");
+    }
+
+    @Override
+    protected int provideBulletsPerShot() {
+        return 2;
+    }
+
+    @Override
+    protected int provideBulletCooldown() {
+        return 2;
+    }
+
+    @Override
+    protected @NotNull Integer provideCustomModelData() {
+        return 26;
+    }
+}
