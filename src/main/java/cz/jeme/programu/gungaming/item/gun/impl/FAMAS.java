58e8a270a0d40e7514c75156043de114e2c89ba6
Mandlemankiller <mandlemankiller@email.cz>
2024-09-02 17:15:31 +0200

    add FAMAS

---
+++ src/main/java/cz/jeme/programu/gungaming/item/gun/impl/FAMAS.java
@@ -0,0 +1,86 @@
+package cz.jeme.programu.gungaming.item.gun.impl;
+
+import cz.jeme.programu.gungaming.item.ammo.Ammo;
+import cz.jeme.programu.gungaming.item.ammo.impl.FiveFiveSixMillimeter;
+import cz.jeme.programu.gungaming.item.gun.Gun;
+import cz.jeme.programu.gungaming.loot.Rarity;
+import net.kyori.adventure.key.KeyPattern;
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
+
+public class FAMAS extends Gun {
+    @Override
+    protected int provideMaxAmmo() {
+        return 25;
+    }
+
+    @Override
+    protected int provideShootCooldown() {
+        return 8;
+    }
+
+    @Override
+    protected int provideReloadDuration() {
+        return 45;
+    }
+
+    @Override
+    protected double provideDamage() {
+        return 3.91;
+    }
+
+    @Override
+    protected double provideBulletVelocity() {
+        return 7;
+    }
+
+    @Override
+    protected double provideRecoil() {
+        return .05;
+    }
+
+    @Override
+    protected double provideInaccuracy() {
+        return .5;
+    }
+
+    @Override
+    protected @NotNull Class<? extends Ammo> provideAmmoType() {
+        return FiveFiveSixMillimeter.class;
+    }
+
+    @Override
+    protected @NotNull String provideDescription() {
+        return "very accurate 3-round SMG with high DPS";
+    }
+
+    @Override
+    protected @KeyPattern.Value @NotNull String provideKey() {
+        return "famas";
+    }
+
+    @Override
+    protected @NotNull Rarity provideRarity() {
+        return Rarity.EPIC;
+    }
+
+    @Override
+    protected @NotNull Component provideName() {
+        return Component.text("FAMAS");
+    }
+
+    @Override
+    protected int provideBulletsPerShot() {
+        return 3;
+    }
+
+    @Override
+    protected int provideBulletCooldown() {
+        return 2;
+    }
+
+    @Override
+    protected @NotNull Integer provideCustomModelData() {
+        return 24;
+    }
+}
